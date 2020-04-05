d064ca0793de6cefe1fc4ee0aff91d9ef39aca6b
Andreas Arndt <andreas.arndt87@gmail.com>
2020-04-05 05:42:46 +0200

    GUI -> Add City, Suspend City; Database und entsprechende Klassen angepasst, Helper Klasse angelegt

---
+++ forecast_accuracy/Helper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+
+namespace forecast_accuracy
+{
+    static class Helper
+    {
+        public static DateTime RoundDateTimeToHours(DateTime dateTime, int hours)
+        {
+            var currentHours = dateTime.TimeOfDay.TotalHours;
+            var roundedHours = Math.Round(currentHours / hours) * hours;
+            return dateTime.Date.AddHours(roundedHours);
+        }
+
+        public static DateTime RoundTimeOfForecast(DateTime forecastTime, DateTime timeOfRequest)
+        {
+            var timeSpanHours = (forecastTime - timeOfRequest).TotalHours;
+            var rounded = Math.Round(timeSpanHours / 24) * 24;
+            var result = forecastTime.AddHours(-rounded);
+            Console.WriteLine(timeOfRequest + " *** " + forecastTime);
+            Console.WriteLine(result);
+            return result;
+        }
+
+        // Hilfsmethode, konvertiert jede Liste in eine entsprechende DataTable.
+        public static DataTable ConvertToDataTable<T>(List<T> dataList)
+        {
+            PropertyDescriptorCollection props = TypeDescriptor.GetProperties(typeof(T));
+            var dataTable = new DataTable();
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    dataTable.Columns.Add(prop.Name, prop.PropertyType.GetGenericArguments()[0]);
+                }
+                else
+                {
+                    dataTable.Columns.Add(prop.Name, prop.PropertyType);
+                }
+            }
+
+            object[] values = new object[props.Count];
+            foreach (T item in dataList)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
+    }
+}
